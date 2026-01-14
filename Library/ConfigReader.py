ed5db7b519831781cf3f76ad91f3c92cc71de33b
Umanga28 <shivapuri572@gmail.com>
2026-01-14 14:08:52 +0545

    Jenkins Automation

--- Library/ConfigReader.py
+++ Library/ConfigReader.py
@@ -1,11 +1,21 @@
 import configparser
-def configRead(section,key):
-    config=configparser.ConfigParser()
-    config.read('../ConfigurationFiles/Config.cfg')
-    return config.get(section,key)
+import os
 
-def ElementsRead(section,key):
-    config=configparser.ConfigParser()
-    config.read('../ConfigurationFiles/Elements.cfg')
-    return config.get(section,key)
+BASE_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..")
+)
+
+def configRead(section, key):
+    config = configparser.ConfigParser()
+    config_path = os.path.join(BASE_DIR, "ConfigurationFiles", "Config.cfg")
+    config.read(config_path)
+    return config.get(section, key)
+
+def ElementsRead(section, key):
+    config = configparser.ConfigParser()
+    elements_path = os.path.join(BASE_DIR, "ConfigurationFiles", "Elements.cfg")
+    config.read(elements_path)
+    return config.get(section, key)
+
+
 #print(configRead('Details','APP_URL'))
