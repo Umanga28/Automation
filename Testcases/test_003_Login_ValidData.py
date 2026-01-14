02272c4e04ee703407904f539187d09a1a9117c2
Umanga28 <shivapuri572@gmail.com>
2026-01-14 14:24:36 +0545

    Jenkins Automation

--- Testcases/test_003_Login_ValidData.py
+++ Testcases/test_003_Login_ValidData.py
@@ -1,7 +1,11 @@
-from Base.InitiateDriver import startBrowser,closeBroswser
+#from Base.InitiateDriver import startBrowser,closeBroswser
+from selenium.webdriver import Chrome
+
 def test_LoginInvalid_Data():
-    driver=startBrowser()
+    driver=Chrome()
+    driver.get("https://facebook.com/login")
     driver.find_element('xpath',"//input[@name='email']").send_keys("as==")
     driver.find_element('xpath',"//input[@name='pass']").send_keys("Uman")
     driver.find_element('link text','Create new account').click()
-    closeBroswser()
+    #closeBrowser()
+    driver.close()
